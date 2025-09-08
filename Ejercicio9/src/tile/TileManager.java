4972eb824c58fecc05ff16d10498b5a8fcd33f63
Pocoloco115 <waltorres@uamv.edu.ni>
2025-09-07 20:09:51 -0600

    tic tac toe

--- Ejercicio9/src/tile/TileManager.java
+++ Ejercicio9/src/tile/TileManager.java
@@ -8,7 +8,7 @@ import javax.imageio.ImageIO;
 import java.io.IOException;
 
 public class TileManager {
-        private Tile[] tiles;
+    private Tile[] tiles;
     public TileManager() {
         tiles = new Tile[8];
         getImage();
@@ -21,7 +21,7 @@ public class TileManager {
         try{
             tiles[0] = new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/table.png")), GameConstants.getTileScale() * 8);
             tiles[1] = new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/x.png")), GameConstants.getTileScale(), "X");
-            tiles[2] = new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/0.png")), GameConstants.getTileScale(), "O");
+            tiles[2] = new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/O.png")), GameConstants.getTileScale(), "O");
             tiles[3] = new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/horizontal.png")), GameConstants.getTileScale());
             tiles[4] = new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/vertical.png")), GameConstants.getTileScale());
             tiles[5] = new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/diagonal_1.png")), GameConstants.getTileScale());
